40f3c98a0d55b6028297695610aa9793bab5f7c6
brysah <brysahvidal@gmail.com>
2024-04-10 18:43:55 -0300

    feat: add back button

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import styles from './User.module.scss'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 export function User() {
 
     const query = useParams();
     const [user, setUser] = useState(null);
- 
+
     useEffect(() => {
         const users = JSON.parse(localStorage.getItem('dataset'));
         const userFiltered = users.filter(user => user.login.uuid === query.id)[0];
@@ -19,17 +19,18 @@ export function User() {
             {
                 user && (
                     <>
+                        <Link to="/" > <button className={styles.container__button}>Voltar</button></Link>
                         <div className={styles.container__map}>
-                        <iframe 
-                        src={`
+                            <iframe
+                                src={`
                         https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d0!2d{${user.location.longitude}}!3d{${user.location.latitude}}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDU5JzQzLjUiUyA2wrAxNycyNC42Ilc!5e0!3m2!1sen!2sus!4v1646761540177!5m2!1sen!2sus
                         `}
-                        width="100%" 
-                        height="100%"   
-                        allowFullScreen="true" 
-                        loading="lazy" 
-                        referrerPolicy="no-referrer-when-downgrade"> 
-                        </iframe>
+                                width="100%"
+                                height="100%"
+                                allowFullScreen="{true}"
+                                loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade">
+                            </iframe>
                         </div>
                         <div className={styles.container__content}>
                             <div className={styles.content__wrapper}>
