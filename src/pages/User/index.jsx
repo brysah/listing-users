7e766000ef98f788bf487158f70413b90dde552b
brysah <brysahvidal@gmail.com>
2024-04-10 18:34:28 -0300

    feat: add map location with user data

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -6,8 +6,7 @@ export function User() {
 
     const query = useParams();
     const [user, setUser] = useState(null);
-
-    console.log(user);
+ 
     useEffect(() => {
         const users = JSON.parse(localStorage.getItem('dataset'));
         const userFiltered = users.filter(user => user.login.uuid === query.id)[0];
@@ -20,7 +19,18 @@ export function User() {
             {
                 user && (
                     <>
-                        <div className={styles.container__map}></div>
+                        <div className={styles.container__map}>
+                        <iframe 
+                        src={`
+                        https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d0!2d{${user.location.longitude}}!3d{${user.location.latitude}}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDU5JzQzLjUiUyA2wrAxNycyNC42Ilc!5e0!3m2!1sen!2sus!4v1646761540177!5m2!1sen!2sus
+                        `}
+                        width="100%" 
+                        height="100%"   
+                        allowFullScreen="true" 
+                        loading="lazy" 
+                        referrerPolicy="no-referrer-when-downgrade"> 
+                        </iframe>
+                        </div>
                         <div className={styles.container__content}>
                             <div className={styles.content__wrapper}>
                                 <img
@@ -35,7 +45,7 @@ export function User() {
                                 </p>
                             </div>
                             <p className={`${styles.content__text} ${styles['content__text--medium']}`}>
-                                {user.name.first} 
+                                {user.name.first}
                                 Lives in {user.location.city}, {user.location.state}
                             </p>
                         </div>
